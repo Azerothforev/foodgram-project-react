64a0252a834e4e73be37cf71420e2a805c37c971
Dmitriy Shalenko <dmitriy.shalenko7@gmail.com>
2023-07-24 05:16:33 +0300

    last

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'foodgramm.ddnsking.com']
 
