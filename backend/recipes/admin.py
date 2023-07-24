2bc864eab7b3e176855a01155032d4a691d58501
Dmitriy Shalenko <dmitriy.shalenko7@gmail.com>
2023-07-24 06:06:38 +0300

    last

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,9 +5,6 @@ from .models import (
     Follow, ShopingCart, IngredientInRecipe
 )
 
-from django.contrib import admin
-from .models import Recipe
-
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
