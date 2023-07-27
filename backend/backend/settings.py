d9c79c6572610f0720e29fa497c691a06a372195
Dmitriy Shalenko <dmitriy.shalenko7@gmail.com>
2023-07-27 06:24:59 +0300

    last

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = get_bool_env('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 AUTH_USER_MODEL = 'users.FoodgramUser'
 
